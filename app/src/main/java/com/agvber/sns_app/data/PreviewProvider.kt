d6e89fceb61099720a92e7c675bcef7831251215
agvber <agvber12@gmail.com>
2024-04-01 18:14:44 +0900

    add: post preview 데이터 추가

--- app/src/main/java/com/agvber/sns_app/data/PreviewProvider.kt
+++ app/src/main/java/com/agvber/sns_app/data/PreviewProvider.kt
@@ -1,5 +1,23 @@
 package com.agvber.sns_app.data
 
+import com.agvber.sns_app.R
+import com.agvber.sns_app.model.Post
+import java.time.LocalDateTime
+
 object PreviewProvider {
 
+    val posts = listOf<Post>(
+        Post(
+            id = 1,
+            image = R.drawable.ic_launcher_foreground,
+            content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt #hashtag",
+            time = LocalDateTime.now()
+        ),
+        Post(
+            id = 2,
+            image = R.drawable.ic_launcher_foreground,
+            content = "Digital goodies designer @pixsellz \nEverything is designed.",
+            time = LocalDateTime.now()
+        )
+    )
 }
