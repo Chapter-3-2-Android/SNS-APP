e6f833423dad535ea9c516eabad54eb2d803f0b1
rlaxodud214 <rlaxodud214@naver.com>
2024-04-02 17:19:36 +0900

    feat: GridView UI 및 기능 구현

--- app/src/main/java/com/agvber/sns_app/MainActivity.kt
+++ app/src/main/java/com/agvber/sns_app/MainActivity.kt
@@ -2,9 +2,25 @@ package com.agvber.sns_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.agvber.sns_app.adapter.PostAdapter
+import com.agvber.sns_app.data.PreviewProvider
 import com.agvber.sns_app.databinding.ActivityMainBinding
+import com.agvber.sns_app.model.Post
 
 class MainActivity : AppCompatActivity() {
+    val posts: List<Post> by lazy {
+        // TODO(setUser) : [for test] 추후 로그인 탭에서 intent로 userID 받은 뒤 수정할 예정,
+        MemoryStorage.setUser(
+            PreviewProvider.users[0]
+        )
+
+        val user = MemoryStorage.getUser()
+
+        PreviewProvider.posts.filter {
+            it.userId == user.id
+        }
+    }
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +28,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initPost()
+    }
+
+    private fun initPost() {
+        val postAdapter = PostAdapter(this, posts)
+        binding.gvPosts.adapter = postAdapter
     }
 }
