2d27928db3313e4871f76e46359588bef9048453
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-04-02 20:47:51 +0900

    메인페이지 구현 (#15)
    * feat: user 프로필 상단 UI 구현
    
    * feat: 메인 페이지 UI 구현
    
    * feat: GridView UI 및 기능 구현
    
    * feat: initView code 추가, 로그인에서 받아오는 userID intent 적용
    
    style(xml): 컴포넌트의 잘못된 id값 수정
    refactor(PreviewProvider): 테스트를 위해서 posts 객체 수정 및 추가, users 객체 수정
    fix(User): user의 프로필 이미지인 image: int 프로퍼티 추가
    
    * refactor: idea 파일 제거
    
    * refactor: xml id 오타 수정, gridview item 간격 수정

--- app/src/main/java/com/agvber/sns_app/MainActivity.kt
+++ app/src/main/java/com/agvber/sns_app/MainActivity.kt
@@ -2,9 +2,25 @@ package com.agvber.sns_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.agvber.sns_app.adapter.PostAdapter
+import com.agvber.sns_app.data.PreviewProvider
 import com.agvber.sns_app.databinding.ActivityMainBinding
+import com.agvber.sns_app.model.Post
+import com.agvber.sns_app.model.User
 
 class MainActivity : AppCompatActivity() {
+    val user: User by lazy {
+        MemoryStorage.getUser()
+    }
+
+    val posts: List<Post> by lazy {
+        val user = MemoryStorage.getUser()
+
+        PreviewProvider.posts.filter {
+            it.userId == user.id
+        }
+    }
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +28,47 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setUserStorage()
+        initView()
+    }
+
+    private fun initView() {
+        initUserProfileImage()
+        initUserName()
+        initUserBio()
+        initPostAdapter()
+    }
+
+    private fun setUserStorage() {
+        // 로그인 페이지에서 userID 값을 넘겨받은 경우
+        if (intent.hasExtra("userID")) {
+            val userid = intent.getStringExtra("userID")
+
+            val user = PreviewProvider.users.filter {
+                it.id == userid
+            } as User
+
+            MemoryStorage.setUser(user)
+        } else { // 로그인 페이지에서 userID 값을 넘겨받지 못한 경우 default 설정
+            MemoryStorage.setUser(PreviewProvider.users[0])
+        }
+    }
+
+    private fun initUserProfileImage() {
+        binding.civProfileImage.setImageResource(user.image)
+    }
+
+    private fun initUserName() {
+        binding.tvUsernameTitle.text = user.name
+        binding.tvUsernameSub.text = user.name
+    }
+
+    private fun initUserBio() {
+        binding.tvBio.text = user.bio
+    }
+
+    private fun initPostAdapter() {
+        val postAdapter = PostAdapter(this, posts)
+        binding.gvPosts.adapter = postAdapter
     }
 }
