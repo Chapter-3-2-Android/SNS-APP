41c8e69982b8bed5417f42d2a0cea2ca5de6b6e2
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-04-02 20:50:39 +0900

    Revert "메인페이지 구현 (#15)"
    This reverts commit 2d27928db3313e4871f76e46359588bef9048453.

--- app/src/main/java/com/agvber/sns_app/model/User.kt
+++ app/src/main/java/com/agvber/sns_app/model/User.kt
@@ -1,14 +1,12 @@
 package com.agvber.sns_app.model
 
 import android.os.Parcelable
-import androidx.annotation.DrawableRes
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class User(
     val id: String,
     val password: String,
-    @DrawableRes val image: Int,
     var name: String,
     var phoneNumber: String?,
     var email: String?,
