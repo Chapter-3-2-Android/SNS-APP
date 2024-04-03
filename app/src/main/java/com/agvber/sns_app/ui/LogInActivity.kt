ea265f3ecca83058783349214821e977766f7cf4
seotoung19920921 <meba22@naver.com>
2024-04-03 22:49:36 +0900

    Add code

--- app/src/main/java/com/agvber/sns_app/ui/LogInActivity.kt
+++ app/src/main/java/com/agvber/sns_app/ui/LogInActivity.kt
@@ -1,10 +1,10 @@
 package com.agvber.sns_app.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.agvber.sns_app.databinding.ActivityLogInBinding
-import com.agvber.sns_app.ui.main.MainActivity
 
 class LogInActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLogInBinding
@@ -14,31 +14,19 @@ class LogInActivity : AppCompatActivity() {
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener {
-            val myIntent = Intent(this, MainActivity::class.java)
-            myIntent.putExtra("userID", "test1")
+        val userId = binding.tvId.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        if (userId.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "아이디와 비밀번호를 모두 입력하세요", Toast.LENGTH_SHORT).show()
+        } else {
+            val myIntent = Intent(this, MyActivity::class.java)
             startActivity(myIntent)
+        }
 
-            val userId = binding.tvId.text.toString()
-            val password = binding.etPassword.text.toString()
-
-            if (userId.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "아이디와 비밀번호를 모두 입력하세요", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "로그인 완료", Toast.LENGTH_SHORT).show()
-            }
+        binding.tvSignup.setOnClickListener {
+            val signupIntent = Intent(this, SignupActivity::class.java)
+            startActivity(signupIntent)
         }
     }
 }
-
-
-//
-//        val signupIntent = Intent(this, SignupActivity::class.java)
-//        startActivity(signupIntent)
-//
-////
-//        val myIntent = Intent(this, MyActivity::class.java)
-//        myIntent.putExtra("string", "string")
-//        startActivity(myIntent)
-//    }
-//}
