49985383196c43d89b04ef75785089a379741792
seoyoung19920921 <161271672+seoyoung19920921@users.noreply.github.com>
2024-04-04 15:08:16 +0900

    middle (#42)
    * style: 로그인 페이지에 필요한 아이콘 추가
    
    * feat: 로그인 화면 구현
    
    * Add : 배경 수정했습니다
    
    * 토스트 추가
    
    * 커밋
    
    * Add code
    
    * middle
    
    * Merge
    
    ---------
    
    Co-authored-by: agvber <agvber12@gmail.com>

--- app/src/main/java/com/agvber/sns_app/ui/LogInActivity.kt
+++ app/src/main/java/com/agvber/sns_app/ui/LogInActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.agvber.sns_app.R
+import com.agvber.sns_app.data.PreviewProvider
 import com.agvber.sns_app.databinding.ActivityLogInBinding
 import com.agvber.sns_app.ui.main.MainActivity
 
@@ -14,31 +16,44 @@ class LogInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+// "Log in" 버튼이 눌렸을 때 호출되는 로직
+
 
         binding.btnLogin.setOnClickListener {
-            val myIntent = Intent(this, MainActivity::class.java)
-            myIntent.putExtra("userID", "test1")
-            startActivity(myIntent)
+            // 호출 됐을 때, editText의 userId와 password값을 가져온다.
+            val userId = binding.etId.text.toString() // test1
+            val password = binding.etPassword.text.toString() // test1p
 
-            val userId = binding.tvId.text.toString()
-            val password = binding.etPassword.text.toString()
+            // PreviewProvider.users list의 id값과 입력받은 userId 값을 비교한다.
+            // 비교해서 일치하는 데이터가 있으면, User 객체를 가져온다.
+            // 데이터가 없다면, null로 collectUser 초기화
+            val collectUser = PreviewProvider.users.filter {
+                it.id == userId
+            }.firstOrNull()
 
-            if (userId.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "아이디와 비밀번호를 모두 입력하세요", Toast.LENGTH_SHORT).show()
+            // collectUser : 제대로 User 객체를 가져왔는지 체크한다.
+            if (collectUser != null) {
+                // User 객체의 비밀번호와 입력받은 UserPassword가 일치하는지 판단한다.
+
+                // 일치한다면, 로그인 성공 로직을 구현한다.
+                if (collectUser.password == password) {
+                    Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("userID", userId)
+
+                    startActivity(intent)
+                }
+                // 일치하지 않는다면, 비밀번호가 일치하지 않는다는 메시지를 띄운다.
+                else {
+                    Toast.makeText(this,(R.string.toast_wrong_id), Toast.LENGTH_SHORT).show()
+                }
             } else {
-                Toast.makeText(this, "로그인 완료", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, (R.string.toast_wrong_ps), Toast.LENGTH_SHORT).show()
             }
+        }
+        binding.tvSignup.setOnClickListener {
+            val signupIntent = Intent(this, SignupActivity::class.java)
+            startActivity(signupIntent)
         }
     }
 }
-
-//
-//        val signupIntent = Intent(this, SignupActivity::class.java)
-//        startActivity(signupIntent)
-//
-////
-//        val myIntent = Intent(this, MyActivity::class.java)
-//        myIntent.putExtra("string", "string")
-//        startActivity(myIntent)
-//    }
-//}
