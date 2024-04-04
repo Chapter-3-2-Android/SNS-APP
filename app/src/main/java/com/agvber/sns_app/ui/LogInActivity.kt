ca80aee935fac0dd0db15a5a4f6649538e99f80d
seotoung19920921 <meba22@naver.com>
2024-04-04 15:01:29 +0900

    스트링 수정

--- app/src/main/java/com/agvber/sns_app/ui/LogInActivity.kt
+++ app/src/main/java/com/agvber/sns_app/ui/LogInActivity.kt
@@ -43,12 +43,11 @@ class LogInActivity : AppCompatActivity() {
 
                     startActivity(intent)
                 }
-                // 일치하지 않는다면, 비밀번호가 일치하지 않는다는 메시지를 띄운다.
                 else {
-                    Toast.makeText(this,(R.string.toast_wrong_id), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this,(R.string.ts_login_id), Toast.LENGTH_SHORT).show()
                 }
             } else {
-                Toast.makeText(this, (R.string.toast_wrong_ps), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, (R.string.ts_login_id), Toast.LENGTH_SHORT).show()
             }
         }
         binding.tvSignup.setOnClickListener {
